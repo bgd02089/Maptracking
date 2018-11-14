30bd690ca29bd7cbf65eb722b3bfe12283a6f103
soyoung <bgd02089@hanmail.net>
2018-11-14 22:40:22 +0900

    1234

--- app/src/main/java/maptracking/example/com/maptracking/MapsActivity.java
+++ app/src/main/java/maptracking/example/com/maptracking/MapsActivity.java
@@ -1,18 +1,41 @@
 package maptracking.example.com.maptracking;
 
-import android.app.Activity;
-import android.support.v4.app.ActivityCompat;
-import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.content.Intent;
-import android.location.LocationManager;
+
 import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MapsActivity extends Activity {
-    private static final int PERMISSIONS_REQUEST = 100;
+import com.google.type.LatLng;
 
+import java.text.NumberFormat;
+import java.util.ArrayList;
+
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+
+public class MapsActivity extends FragmentActivity implements OnMap {
+
+    private static final int PERMISSIONS_REQUEST = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +47,10 @@ public class MapsActivity extends Activity {
         if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             finish();
         }
+
+
         //Check whether this app has access to the location permission//
+
 
         int permission = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION);
@@ -42,6 +68,45 @@ public class MapsActivity extends Activity {
                     PERMISSIONS_REQUEST);
         }
     }
+
+
+    private void getSampleMarkerItems() {
+        ArrayList<MarkerItem> sampleList = new ArrayList();
+
+
+        sampleList.add(new MarkerItem(37.538523, 126.96568));
+        sampleList.add(new MarkerItem(37.527523, 126.96568));
+        sampleList.add(new MarkerItem(37.549523, 126.96568));
+        sampleList.add(new MarkerItem(37.538523, 126.95768));
+
+
+        for (MarkerItem markerItem : sampleList) {
+            addMarker(markerItem, false);
+        }
+
+    }
+    private void setCustomMarkerView() {
+
+        marker_root_view = LayoutInflater.from(this).inflate(R.layout.activity_maps, null);
+        tv_marker = (ImageView) marker_root_view.findViewById(R.id.tv_marker);
+    }
+
+    private Marker addMarker(MarkerItem markerItem, boolean isSelectedMarker) {
+
+
+        LatLng position = new LatLng(markerItem.getLat(), markerItem.getLon());
+        String formatted = NumberFormat.getCurrencyInstance().format((price));
+
+        tv_marker.setText(formatted);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title(Integer.toString(price));
+        markerOptions.position(position);
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(createDrawableFromView(this, marker_root_view)));
+
+        return mMap.addMarker(markerOptions);
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[]
@@ -76,4 +141,6 @@ public class MapsActivity extends Activity {
 
         finish();
     }
+
+
 }
